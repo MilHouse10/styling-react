f6cb260b56313598512427cfc28838be47210adb
MilHouse10 <alstjr_96@naver.com>
2021-06-09 17:43:27 +0900

    ㅡㄷ얌

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import CSSModule from './CSSModule';
+import StyledComponent from './StyledComponent';
 
 class App extends Component {
   render() {
     return (
-      <CSSModule />
+      <div>
+        <CSSModule />
+        <StyledComponent />
+      </div>
     );
   }
 }
